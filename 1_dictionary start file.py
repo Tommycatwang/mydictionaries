fafe38ef376809ef16b20e8234dc30884cb90817
tommy <tommy@test.com>
2022-09-08 12:14:30 -0500

    this is the class work

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -10,10 +10,18 @@ print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)
+print(type(phonebook))
+phone = phonebook['Chris']
+print(phone)
+print(len(phonebook))
 
+mydictionary = dict(m=8,n=9)
+print(mydictionary)
 
-
-
+#if you want to clear dictionary
+mydict={}
+print(mydict)
 print()
 print('*****  end section 1 ********')
 print()
@@ -25,16 +33,21 @@ print()
 print()
 print('*****  start section 2 - search dictionary ********')
 print()
+'''
+name = 'chris'
+if name in phonebook:
+    print(phonebook[name])
+else:
+    print(name, 'not found')
 
 
 
-
-
-
+'''
 
 print()
 print('*****  end section 2 ********')
 print()
+'''
 
 
 
@@ -45,11 +58,14 @@ print()
 print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
+print(phonebook)
+phonebook['Chris'] = '555-0123'
+phonebook['Joe'] = '555-4444'
+
+print(phonebook)
 
 
-
-
-
+'''
 print()
 print('*****  end section 3 ********')
 print()
@@ -62,28 +78,36 @@ print()
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
-
+'''
+#del phonebook ['Chris']
+#print(phonebook)
 
 
 
 print()
 print('*****  end section 4 ********')
 print()
+'''
 
-
-
+'''
 
 
 
 print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
+#it just iterate for the key, if you want to iterate other things, you need to 
+for k in phonebook:
+    print(k)
+    print(phonebook[k])
 
+for value in phonebook.values():
+    print(value)#it will print out the value on the key
 
-
-
-
-
+for k,v in phonebook.items():
+    print('key:',k,'     value:  ',v)
+for tuple in phonebook.items():
+    print(tuple)#it will return the tuple 
 print()
 print('*****  end section 5 ********')
 print()
@@ -96,9 +120,12 @@ print()
 print('*****  start section 6 - using get and clear ********')
 print()
 
+phone = phonebook.get("chris","key not found")#if get it, apply on left, otherwise on right
+print(phone)
 
-
-
+#phonebook.clear()
+#print(phonebook)
+#which will clear phonebook content
 
 
 print()
@@ -111,6 +138,9 @@ print()
 print('*****  start section 7 - using pop method ********')
 print()
 
+#remove=phonebook.pop('Chris','key not found')
+#print(remove)
+#print(phonebook)
 
 
 
@@ -121,12 +151,15 @@ print('*****  end section 7 ********')
 print()
 
 
-
 print()
 print('*****  start section 8 - using popitem ********')
 print()
 
+#select the random 
 
+#a = phonebook.popitem()#it suppose to pick up the random, but it pick the last item 
+#print(a)
+#print(phonebook)
 
 
 
@@ -141,16 +174,18 @@ print()
 print('*****  start section 9 - using random and converting to list ********')
 print()
 
+list_of_keys = list(phonebook)#
+print(list_of_keys)
+random_key = random.choice(list_of_keys)
+print(random_key)
+print(phonebook[random_key])
 
-
-
-
+print(phonebook[random.choice(list(phonebook))])#exact same thing on the upper list.
 print()
 print('*****  end section 9 ********')
 print()
 
 
-'''
 
 
 

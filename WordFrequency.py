22b44f4f4fb190623e122ddd1c8cb4471b03cffd
tommy <tommy@test.com>
2022-09-20 23:55:50 -0500

    class need

---
+++ WordFrequency.py
@@ -0,0 +1,14 @@
+readbook=open('sometext.txt','r')
+list_words={}
+for wordon in readbook:
+    wordon =wordon.lower()
+    wordread = wordon.split(" ")
+    for wordreadd in wordread:
+        if wordreadd in list_words:
+            list_words[wordreadd]= list_words[wordreadd] +1
+
+        else:
+            list_words[wordreadd] = 1
+
+for read in list_words.keys():
+    print(read + ',' + str(list_words[read]))
